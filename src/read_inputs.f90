f32f6af763c6b52747d5120c26ed14a6614737ea
jjmaldonis <jjmaldonis@gmail.com>
2016-01-19 12:31:58 -0600

    completely rewrote the parameter file inputs and the fem file data read-in

--- src/read_inputs.f90
+++ src/read_inputs.f90
@@ -21,19 +21,17 @@ contains
         real, intent(out) :: q
         real, intent(out) :: scale_fac
         integer, intent(out) :: status2
-        integer :: nelements
 
         ! Local variables
-        character (len=256) comment
-        character (len=256) scatteringfile ! The scattering file name
-        integer filenamelength ! The file name length in scatteringfile or femfile
-        integer i
-        real indicator_end ! Indicator_end=-1 means the end of file reaches
-        integer status1 ! Indicate the status of opening file in this subroutine
-        logical file_end ! Indicate fileend has reached
-        integer num_line  ! Number of lines in each data file except comment line
+        character (len=256) :: comment
+        character (len=256) :: line
+        integer :: nelements
+        integer :: filenamelength ! The file name length in scatteringfile or femfile
+        integer :: i, line_size ! Counter i; number of numbers on a line
+        integer :: status1 ! Indicate the status of opening file in this subroutine
+        integer :: num_line  ! Number of lines in each data file except comment line
         real, pointer, dimension(:) :: tempdata ! Temperature data
-        integer stat_allocate1, stat_allocate2, stat_allocate3, stat_allocate4 ! Allocate status, 0 means success
+        integer :: stat_allocate1, stat_allocate2, stat_allocate3, stat_allocate4 ! Allocate status, 0 means success
 
         open(20, file=param_filename,iostat=status2, status='old')
         if(status2 .ne. 0) then !open fails
@@ -44,63 +42,106 @@ contains
         read(20, '(A256)') model_fn; model_fn = adjustl(model_fn)
         read(20, '(A256)') femfile; femfile = adjustl(femfile)
         read(20, '(A256)') eam_file; eam_file= adjustl(eam_file)
-        read(20, *) step_start, step_end
-        read(20, *) temperature, max_move, temp_move_decrement
-        read(20, * ) nelements
+        read(20, '(A)') line
+        read(line, *) step_start, step_end
+        read(20, '(A)') line; if(index(line, "#") .ne. 0) then; line = line(1:index(line, "#")-1); line = adjustl(line); endif
+        read(line, *) temperature, max_move, temp_move_decrement
+        read(20, '(A)') line; if(index(line, "#") .ne. 0) then; line = line(1:index(line, "#")-1); line = adjustl(line); endif
+        read(line, * ) nelements
         allocate(cutoff_r(nelements,nelements))
-        read(20, *) cutoff_r ! This is a nelements by nelements matrix
-        read(20, *) iseed2
-        read(20, *) alpha
-        read(20, *) scale_fac
-        read(20, *) nphi, npsi, ntheta
-        read(20, *) q
+        do i=1, nelements
+            read(20, '(A)') line; if(index(line, "#") .ne. 0) then; line = line(1:index(line, "#")-1); line = adjustl(line); endif
+            read(line, *) cutoff_r(:,i) ! This is a nelements by nelements matrix
+        end do
+        read(20, '(A)') line; if(index(line, "#") .ne. 0) then; line = line(1:index(line, "#")-1); line = adjustl(line); endif
+        read(line, *) iseed2
+        read(20, '(A)') line; if(index(line, "#") .ne. 0) then; line = line(1:index(line, "#")-1); line = adjustl(line); endif
+        read(line, *) alpha
+        read(20, '(A)') line; if(index(line, "#") .ne. 0) then; line = line(1:index(line, "#")-1); line = adjustl(line); endif
+        read(line, *) scale_fac
+        read(20, '(A)') line; if(index(line, "#") .ne. 0) then; line = line(1:index(line, "#")-1); line = adjustl(line); endif
+        read(line, *) nphi, npsi, ntheta
+        read(20, '(A)') line; if(index(line, "#") .ne. 0) then; line = line(1:index(line, "#")-1); line = adjustl(line); endif
+        read(line, *) q
         close(20)
 
-        num_line = 0
-        filenamelength=len_trim(femfile)
-        file_end=.false.
-        open(30,file=femfile(1:filenamelength),iostat=status1,status='old') 
+        if(index(model_fn, "#") .ne. 0) then
+            model_fn = model_fn(1:index(model_fn, "#")-1)
+            model_fn = adjustl(model_fn)
+        endif
+        if(index(femfile, "#") .ne. 0) then
+            femfile = femfile(1:index(femfile, "#")-1)
+            femfile = adjustl(femfile)
+        endif
+        if(index(eam_file, "#") .ne. 0) then
+            eam_file = eam_file(1:index(eam_file, "#")-1)
+            eam_file = adjustl(eam_file)
+        endif
+
+        filenamelength = len_trim(femfile)
+        open(30, file=femfile(1:filenamelength), iostat=status1, status='old') 
         if(status1 .eq. 0) then ! Open succeeds
             read(30, '(a80)') comment ! First line is comment
-            ! Count how many data pairs are in the file. -1 denotes EOF
-            do while( .not. file_end)
-                read(30, *) indicator_end
-                if(abs(indicator_end+1.0) .le. 1e-6) then !file end is reached
-                    exit !exit this loop
-                else
-                    num_line = num_line + 1
+            num_line = 0
+            do while(.true.) ! Until end of file
+                read(30, '(A)', iostat=status1) line
+                if(status1 .ne. 0) exit
+                if(num_line .gt. 0) then
+                    if(line_size .ne. count_numbers_on_line(line)) then
+                        stop "Each line must have the same number of elements on it in your FEM data file!"
+                    endif
                 endif
+                line_size = count_numbers_on_line(line)
+                line = adjustl(line)
+                num_line = num_line + 1
             enddo
-            rewind(30) !go to the beginning of the file
-            read(30, '(a80)') comment
-            allocate(tempdata(4*num_line),stat=stat_allocate1)
-            !read k ,v, and v_err data
-            !read k first, then v, last v_err
-            if(stat_allocate1 .eq. 0) then
-                read(30, *) tempdata
-                allocate(k(num_line), stat=stat_allocate2)
-                allocate(v(num_line), stat=stat_allocate3)
-                allocate(v_err(num_line), stat=stat_allocate4)
-                allocate(v_background(num_line))
 
-                if ((stat_allocate2 .eq. 0) .and. (stat_allocate3 .eq. 0) .and. (stat_allocate4 .eq. 0)) then
-                    k = tempdata(1:4*num_line:4)
-                    v = tempdata(2:4*num_line:4)
-                    v_err = tempdata(3:4*num_line:4)
-                    v_background = tempdata(4:4*num_line:4)
-                else
-                    print *, 'fem part 2 or 3, or 4 fails!'
-                    return
-                endif !allocate2, 3 and 4
-            else
-                print *, 'fem part allocation fail'
-                return
+            if(line_size .ge. 1) then  ! k data only (for FEMSIM)
+                allocate(k(num_line))
             endif
-        deallocate(tempdata)
+            if(line_size .ge. 3) then  ! k, vk, and vkerr
+                allocate(v(num_line))
+                allocate(v_err(num_line))
+            endif
+            if(line_size .ge. 4) then  ! k, vk, vkerr, and background vk
+                allocate(v_background(num_line))
+            endif
+
+            rewind(30) ! Go to the beginning of the file
+            read(30, '(a80)') comment
+
+            write(*,*) line_size
+            do i=1, num_line
+                write(*,*) i
+                if(line_size .eq. 1) then  ! k data only (for FEMSIM)
+                    read(30, *) k(i)
+                else if(line_size .eq. 3) then  ! k, vk, and vkerr
+                    read(30, *) k(i), v(i), v_err(i)
+                else if(line_size .eq. 4) then  ! k, vk, vkerr, and background vk
+                    write(*,*) i
+                    read(30, *) k(i), v(i), v_err(i), v_background(i)
+                endif
+            end do
         else
-            print *, 'open fem file fails', femfile(1:filenamelength)
+            write(*,*) "Failed to open the FEM data file ", femfile(1:filenamelength)
         endif
         close(30)
     end subroutine read_inputs
+
+    function count_numbers_on_line(str) result(nums)
+        character(*) :: str
+        logical :: sep
+        integer :: i, nums
+        sep = .true.
+        nums = 0
+        do i = 1, len(str)
+            if ( (ichar(str(i:i)) .ge. 48 .and. ichar(str(i:i)) .le. 57) .or.  ichar(str(i:i)) .eq. 46 ) then ! we have a number or a .
+                if(sep) nums = nums + 1
+                sep = .false.
+            else
+                sep = .true.
+            end if
+        end do
+    end function
 
 end module readinputs
